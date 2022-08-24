0a2052bc0ff760a7ae5b62f01875857a7d1c9237
Amy McCabe <mcca0168@umn.edu>
2022-08-24 10:43:10 -0500

    lesson 1 complete and working correctly

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,7 +5,7 @@ const PizzaController = {
   // Get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
-      .then(dbPizzaData => json(dbPizzaData))
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
