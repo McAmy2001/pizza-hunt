a62622b7db6a9d575f057cbdd45bff699cd85d79
Amy McCabe <mcca0168@umn.edu>
2022-08-26 13:11:33 -0500

    lesson 4, before heroku deployment

--- models/Comment.js
+++ models/Comment.js
@@ -3,10 +3,25 @@ const dateFormat = require('../utils/dateFormat');
 
 const replySchema = new Schema(
   {
-    replyId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
-    replyBody: { type: String },
-    writtenBy: { type: String },
-    createdAt: { type: Date, default: Date.now, get: createdAtVal => dateFormat(createdAtVal) }
+    replyId: { 
+      type: Schema.Types.ObjectId, 
+      default: () => new Types.ObjectId() 
+    },
+    replyBody: { 
+      type: String,
+      required: true,
+      trim: true 
+    },
+    writtenBy: { 
+      type: String,
+      required: true,
+      trim: true 
+    },
+    createdAt: { 
+      type: Date, 
+      default: Date.now, 
+      get: createdAtVal => dateFormat(createdAtVal) 
+    }
   },
   {
     toJSON: { getter: true }
@@ -16,10 +31,14 @@ const replySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
